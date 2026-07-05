e72dc8b7053088884db36a8785d7ffd865c9bb32
Shreya123khot <shreyakhot366@gmail.com>
2026-07-05 19:05:08 +0530

    Add files via upload

---
+++ p-app.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+model = pickle.load(open("model.pkl", "rb"))
+
+st.title("🎓 Student Placement Prediction System")
+
+cgpa = st.number_input("CGPA", min_value=0.0, max_value=10.0)
+
+internships = st.number_input("Internships", min_value=0)
+
+projects = st.number_input("Projects", min_value=0)
+
+workshops = st.number_input("Workshops/Certifications", min_value=0)
+
+aptitude = st.number_input("Aptitude Test Score", min_value=0, max_value=100)
+
+softskills = st.number_input("Soft Skills Rating", min_value=0.0, max_value=5.0)
+
+extracurricular = st.selectbox(
+    "Extracurricular Activities",
+    [0,1]
+)
+
+training = st.selectbox(
+    "Placement Training",
+    [0,1]
+)
+
+ssc = st.number_input("SSC Marks", min_value=0, max_value=100)
+
+hsc = st.number_input("HSC Marks", min_value=0, max_value=100)
+
+if st.button("Predict"):
+        input_data = np.array([[
+        cgpa,
+        internships,
+        projects,
+        workshops,
+        aptitude,
+        softskills,
+        extracurricular,
+        training,
+        ssc,
+        hsc
+    ]])
+
+prediction = model.predict(input_data)
+
+    if prediction[0] == 1:
+        st.success("🎉 Student is Likely to be Placed")
+    else:
+        st.error("❌ Student is Not Likely to be Placed")
