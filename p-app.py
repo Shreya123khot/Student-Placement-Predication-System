ddc5d69ea6edd98f36172ab79892ca9a69ad360f
Shreya123khot <shreyakhot366@gmail.com>
2026-07-05 19:10:35 +0530

    Update p-app.py

--- p-app.py
+++ p-app.py
@@ -2,10 +2,13 @@ import streamlit as st
 import pickle
 import numpy as np
 
+# Load Model
 model = pickle.load(open("model.pkl", "rb"))
 
+# Title
 st.title("🎓 Student Placement Prediction System")
 
+# Input Fields
 cgpa = st.number_input("CGPA", min_value=0.0, max_value=10.0)
 
 internships = st.number_input("Internships", min_value=0)
@@ -20,20 +23,22 @@ softskills = st.number_input("Soft Skills Rating", min_value=0.0, max_value=5.0)
 
 extracurricular = st.selectbox(
     "Extracurricular Activities",
-    [0,1]
+    [0, 1]
 )
 
 training = st.selectbox(
     "Placement Training",
-    [0,1]
+    [0, 1]
 )
 
 ssc = st.number_input("SSC Marks", min_value=0, max_value=100)
 
 hsc = st.number_input("HSC Marks", min_value=0, max_value=100)
 
+# Prediction
 if st.button("Predict"):
-        input_data = np.array([[
+
+    input_data = np.array([[
         cgpa,
         internships,
         projects,
@@ -46,7 +51,7 @@ if st.button("Predict"):
         hsc
     ]])
 
-prediction = model.predict(input_data)
+    prediction = model.predict(input_data)
 
     if prediction[0] == 1:
         st.success("🎉 Student is Likely to be Placed")
